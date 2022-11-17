ba0d717f84f26a1831b7cabdbd33047838c5eebe
s5443626 <117823998+s5443626@users.noreply.github.com>
2022-11-17 21:10:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main() {
    float a=0,b=0,area=0;
    int opzione=0;
    cin >> a >> b >> opzione;
-   Switch(opzione){
+   switch(opzione){
       case 0:
       area=(a*b)/2;
       cout << area << endl;
